03b9274463c2438c18f86c409cca9837d9bace8c
stana <asta2333@gmail.com>
2017-01-20 10:18:40 +0900

    Init dev.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 # Nflex Training demo script
+from viptela import Viptela
+
+VIPTELA_USERNAME = 'matthew'
+VIPTELA_PSWD = 'R0ug30n3!'
+VIPTELA_SERVER = '54.251.162.192'
 
 def get_resources(event, context):
-    print 'hello from get_resources' 
+    vip_cli = Viptela(VIPTELA_USERNAME, VIPTELA_PSWD, VIPTELA_SERVER) 
+    devices = vip_cli.get_devices()
+    print 'devices - {}'.format(devices)
